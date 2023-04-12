29c825da54680fca99147c3acc0541e2bd1ef399
Marcin Wiącek <marcin@mwiacek.com>
2023-04-12 23:00:08 +0200

    Updates

--- ng1.js
+++ ng1.js
@@ -7,21 +7,152 @@ const url = require('url');
 
 var x = fs.readFileSync(path.normalize(__dirname + "/bela3.json"), 'utf8');
 x = x.charCodeAt(0) == 65279 ? x.substring(1) : x;
-let json = JSON.parse(x)
-console.log(JSON.stringify(json,null,2));
+let jsonObj = JSON.parse(x)
+//console.log(JSON.stringify(json,null,2));
 
-console.log (json.project.name);
-console.log (json.project.testsuite[0].name);
+async function executeRequest(req) {
+  
+  console.log(req);
+    console.log("request " + JSON.stringify(req));
+    var q = url.parse(req.url, true);
+    console.log("url " + JSON.stringify(q));
+    //	if (q.protocol=="https") q.port2 = "443";
+    //	if (q.protocol=="http") q.port2 = "80";
 
-for (let tsnumber in json.project.testsuite) {
-  console.log("testsuite name "+json.project.testsuite[tsnumber].name);
-  for (let tcnumber in json.project.testsuite[tsnumber].testcase) {
-    console.log("testcase name "+json.project.testsuite[tsnumber].testcase[tcnumber].name);
-    for (let stepnumber in json.project.testsuite[tsnumber].testcase[tcnumber].step) {
-      console.log("step name "+json.project.testsuite[tsnumber].testcase[tcnumber].step[stepnumber].name);
-    }
-  }
+    const options = {
+        //	hostname:q.hostname,
+        //	port:443,
+        //	path:q.path,
+        //        method:req.method
+        //        req.headers
+    };
+    console.log(options);
+
+    return new Promise((resolve, reject) => {
+        switch (req.type) {
+            case "http1get":
+                http.get(req.url, (response) => {
+                    let chunks_of_data = [];
+
+                    response.on('data', (fragments) => {
+                        chunks_of_data.push(fragments);
+                    });
+
+                    response.on('end', () => {
+                        let response_body = Buffer.concat(chunks_of_data);
+                        resolve(response_body.toString());
+                    });
+
+                    response.on('error', (error) => {
+                        reject(error);
+                    });
+                });
+                break;
+            case "http1post":
+                http.post(req.url, (response) => {
+                    let chunks_of_data = [];
+
+                    response.on('data', (fragments) => {
+                        chunks_of_data.push(fragments);
+                    });
+
+                    response.on('end', () => {
+                        let response_body = Buffer.concat(chunks_of_data);
+                        resolve(response_body.toString());
+                    });
+
+                    response.on('error', (error) => {
+                        reject(error);
+                    });
+                });
+                break;
+            case "https1get":
+                https.get(req.url, (response) => {
+                    let chunks_of_data = [];
+
+                    response.on('data', (fragments) => {
+                        chunks_of_data.push(fragments);
+                    });
+
+                    response.on('end', () => {
+                        let response_body = Buffer.concat(chunks_of_data);
+                        resolve(response_body.toString());
+                    });
+
+                    response.on('error', (error) => {
+                        reject(error);
+                    });
+                });
+                break;
+            case "https1post":
+                https.post(req.url, (response) => {
+                    let chunks_of_data = [];
+
+                    response.on('data', (fragments) => {
+                        chunks_of_data.push(fragments);
+                    });
+
+                    response.on('end', () => {
+                        let response_body = Buffer.concat(chunks_of_data);
+                        resolve(response_body.toString());
+                    });
+
+                    response.on('error', (error) => {
+                        reject(error);
+                    });
+                });
+                break;
+        }
+    });
 }
+
+async function executetestcase(arr, data) {
+    console.log (arr.name);
+    console.log (arr.testsuite[0].name);
+
+    for (let tsnumber in arr.testsuite) {
+      console.log("testsuite name "+arr.testsuite[tsnumber].name);
+      for (let tcnumber in arr.testsuite[tsnumber].testcase) {
+        console.log("testcase name "+arr.testsuite[tsnumber].testcase[tcnumber].name);
+	let lines = arr.testsuite[tsnumber].testcase[tcnumber].input;
+        let headers = []
+        for (let index2 in lines) {
+              let l = lines[index2];
+              if (headers.length == 0) {
+                  headers = l.split(",");
+              } else {
+                  let ll = l.split(",");
+                  let i = 0;
+                  let arra = [];
+                  headers.forEach(function(h) {
+                     arra[h] = ll[i];
+                      i++;
+                  });
+                  console.log(arra);
+                  for (let stepnumber in arr.testsuite[tsnumber].testcase[tcnumber].step) {
+         	     console.log("step name "+arr.testsuite[tsnumber].testcase[tcnumber].step[stepnumber].name); 
+         	     var stepcopy =JSON.parse(JSON.stringify(arr.testsuite[tsnumber].testcase[tcnumber].step[stepnumber]));
+         	     if (stepcopy.function != null) {
+    continue;
+  }
+         	    
+         	     for (let d in arra) {
+       console.log (d);
+       console.log (arra[d]);
+       stepcopy.url = stepcopy.url.replace("{{"+d+"}}",arra[d]);
+    }
+         	     console.log(stepcopy);
+var response = await    executeRequest(stepcopy);         	     
+console.log(response);
+	          }
+              }
+          };
+       }
+    }
+}
+
+executetestcase(jsonObj.project, []);
+
 
 
 /*
