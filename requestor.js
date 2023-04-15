b96cac2eaf87edaae65884e4700a932e40313247
Marcin Wiącek <marcin@mwiacek.com>
2023-04-16 01:15:31 +0200

    Update

--- requestor.js
+++ requestor.js
@@ -3,14 +3,19 @@
 const fs = require('fs');
 const http = require('http');
 const https = require('https');
-//const http2 = require('http2');
+const http2 = require('http2');
 const path = require('path');
 const url = require('url');
 const {spawn} = require('child_process');
 
+const hostname = '127.0.0.1';
+const port = 3000;
+
 var x = fs.readFileSync(path.normalize(__dirname + "/bela3.json"), 'utf8');
 x = x.charCodeAt(0) == 65279 ? x.substring(1) : x;
 let jsonObj = JSON.parse(x)
+
+var all_responses = []
 
 async function executeRequest(req) {
     console.log(req);
@@ -37,7 +42,6 @@ async function executeRequest(req) {
     }
     console.log(options);
     var agent = new https.Agent(options);
-
 
     return new Promise((resolve, reject) => {
         method(req.url, options, (response) => {
@@ -83,6 +87,7 @@ async function executetestcase(arr, data) {
                     i++;
                 });
                 console.log(arra);
+		all_responses = []
                 for (let stepnumber in arr.testsuites[tsnumber].testcases[tcnumber].steps) {
                     console.log("step name " + arr.testsuites[tsnumber].testcases[tcnumber].steps[stepnumber].name);
                     var stepcopy = JSON.parse(JSON.stringify(arr.testsuites[tsnumber].testcases[tcnumber].steps[stepnumber]));
@@ -106,11 +111,15 @@ async function executetestcase(arr, data) {
                         console.log(arra[d]);
                         stepcopy.url = stepcopy.url.replace("{{" + d + "}}", arra[d]);
                     }
+for (const match of stepcopy.url.matchAll(/{{(.*)#(.*)}}/g)) {
+  console.log(match)
+}
                     await request2(stepcopy);
                 }
             }
         }
     }
+//    console.log(JSON.stringify(all_responses));
 }
 
 function addToLog(str) {
@@ -138,6 +147,8 @@ async function request2(req) {
 	if (response.error) {
             addToLog("  <response_error>" + response.error + "</response_error>\n");
 	} else {
+	    response.name = req.name;
+	    all_responses.push(response);
             for (let headername in response.headers) {
                 addToLog("  <response_header>" + headername + ": " + response.headers[headername] + "</response_header>\n");
             }
@@ -150,7 +161,7 @@ async function request2(req) {
         addToLog("</request>\n");
 }
 
-executetestcase(jsonObj, []);
+//executetestcase(jsonObj, []);
 
 /*
 const ls = spawn('ls', ['/usr']);
@@ -164,3 +175,85 @@ ls.on('close', (code) => {
   console.log("exit"+ code);
 });
 */
+
+const onRequestHandler = (req, res) => {
+/*    if (processExternalFiles(req, res)) return;
+
+    console.log(' ');
+    let cookieSessionToken = "";
+    let userName = null;
+    //console.log(req.headers);
+    if (req.headers['cookie']) {
+        console.log(req.headers['cookie']);
+        req.headers['cookie'].split("; ").forEach(function(cookie) {
+            if (cookie.indexOf("session=") == 0) cookieSessionToken = cookie.substr(8);
+        });
+    }
+    if (cookieSessionToken != "") {
+        for (let index in sessions) {
+            session = sessions[index];
+            //            console.log("mamy sesję1 " + session[SessionField.SessionToken]);
+            if (session[SessionField.Expiry] < Date.now()) {
+                if (session[SessionField.RefreshCallback] != null) clearTimeout(session[SessionField.RefreshCallback]);
+                sessions.splice(index, 1);
+                continue;
+            }
+            //            console.log("mamy sesję2 " + session[SessionField.SessionToken]);
+            if (cookieSessionToken == session[SessionField.SessionToken]) {
+                userName = session[SessionField.UserName];
+                console.log("found user " + userName);
+                session[SessionField.Expiry] += sessionValidity;
+                break;
+            }
+        }
+    }
+    const newCookieSessionToken = (userName == null);
+    if (userName == null) {
+        userName = "";
+        const cookieSessionToken = crypto.randomBytes(32).toString('base64');
+
+        res.setHeader('Set-Cookie', 'session=' + cookieSessionToken + '; SameSite=Strict; Secure');
+
+        // order must be consistent with SessionField
+        sessions.push([cookieSessionToken, Date.now() + sessionValidity, '', null]); // non logged
+
+        console.log("nowa sesja " + cookieSessionToken);
+    }
+    console.log('user name is ' + userName);
+
+    if (req.method === 'GET') {
+        console.log(req.url);
+        const params = url.parse(req.url, true).query;
+        if (params["sse"]) { // PUSH functionality
+            parseGETWithSseParam(req, res, userName, cookieSessionToken);
+            if (newCookieSessionToken) {
+                setTimeout(function() {
+                    sendReloadToPage(res);
+                }, 2000); // 2 seconds
+            }
+        } else if (params["set"]) { // setting cookies with config
+            parseGETWithSetParam(req, res, params);
+        } else if (params["q"]) {
+            parseGETWithQParam(req, res, params, userName);
+        } else {
+            showMainPagePrzyp(req, res, 0, [], userName);
+        }
+    } else if (req.headers['content-type'] == "application/x-www-form-urlencoded" && cookieSessionToken != "") { // POST
+        let body = "";
+        req.on('data', function(data) {
+            body += data;
+            if (body.length > 1e6 * 6) req.connection.destroy(); // 6 MB 
+        });
+        req.on('end', function() {
+            console.log(body);
+            parsePOSTforms(url.parse("/?" + body, true).query, res, userName, cookieSessionToken);
+        });
+    }*/
+};
+
+http2.createSecureServer({
+    key: fs.readFileSync(__dirname + '//internal//localhost-privkey.pem'),
+    cert: fs.readFileSync(__dirname + '//internal//localhost-cert.pem')
+}, onRequestHandler).listen(port, hostname, () => {
+    console.log(`Server running at https://${hostname}:${port}/`);
+});
